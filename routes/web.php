dbbe859ca31b605c11d5737cac60716ea8b47288
verdy-capstone <85613233+verdy-capstone@users.noreply.github.com>
2022-04-05 21:25:18 +0700

    V1
    Create Home page
    Create Model&Migration

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        'title' => 'Home'
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        'title' => 'About'
+    ]);
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/posts', function () {
+    return view('posts');
 });
