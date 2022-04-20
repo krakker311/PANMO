5ab9340e1d9448d7baee093bb4d6221f1e50ad23
Garry <gnovento@gmail.com>
2022-04-20 15:02:33 +0700

    configure view and change param
    configure view and change param

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,20 +2,20 @@
 
 @section('container')
 
-<section class="h-100 gradient-custom-2">
-  <div class="container py-5 h-100">
+<section class="h-100">
+  <div class="container py-4 h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col col-lg-9 col-xl-7">
+      <div class="col col-xl-12 col-xl-7">
         <div class="card">
           <div class="rounded-top text-white d-flex flex-row" style="background-color: #000; height:200px;">
             <div class="ms-4 mt-5 d-flex flex-column" style="width: 150px;">
               <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp" alt="Generic placeholder image" class="img-fluid img-thumbnail mt-4 mb-2" style="width: 150px; z-index: 1">
-              <button type="button" class="btn btn-outline-dark" data-mdb-ripple-color="dark" style="z-index: 1;">
-                Edit profile
+              <button type="button" class="btn btn-outline-primary" style="z-index: 1;">
+                Order Now
               </button>
             </div>
             <div class="ms-3" style="margin-top: 130px;">
-              <h5>Andy Horwitz</h5>
+              <h5>{{ $post->author->name }}</h5>
               <p>New York</p>
             </div>
           </div>
@@ -37,11 +37,9 @@
           </div>
           <div class="card-body p-4 text-black">
             <div class="mb-5">
-              <p class="lead fw-normal mb-1">About</p>
+              <p class="lead fw-normal mb-1">Job Desc: </p>
               <div class="p-4" style="background-color: #f8f9fa;">
-                <p class="font-italic mb-1">Web Developer</p>
-                <p class="font-italic mb-1">Lives in New York</p>
-                <p class="font-italic mb-0">Photographer</p>
+                <p class="font-italic mb-1">{!!$post->body!!}</p>
               </div>
             </div>
             <div class="d-flex justify-content-between align-items-center mb-4">
