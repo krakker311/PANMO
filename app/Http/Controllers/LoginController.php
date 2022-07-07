ad73aa571b790d5ce442b2ee35e43e5717cda9fd
Garry <gnovento@gmail.com>
2022-07-07 21:12:39 +0700

    test api login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -29,7 +29,8 @@ class LoginController extends Controller
         if(Auth::attempt($credentials)){
             $request->session()->regenerate();
 
-            return response()->json(['email' => $request->email, 'success' => 'true']);
+            $output = new Symfony\Component\Console\Output\ConsoleOutput();
+            $output->writeln($request->email);
         }
 
         return response()->json(['success' => 'false']);
