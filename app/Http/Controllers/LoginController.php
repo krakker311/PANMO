5fb0536f6e913b9e57a3cde228176617521811c9
Garry <gnovento@gmail.com>
2022-07-07 20:16:35 +0700

    test api login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Auth;
 class LoginController extends Controller
 {
     public function index(){
-        return view('auth.login', [
+        return view('login-android', [
             'title' => 'Login',
             'active' => 'login'
         ]);
