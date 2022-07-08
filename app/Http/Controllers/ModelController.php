f7695c9e56eb29a34bf76a90debe20ad38d9f6db
Garry <gnovento@gmail.com>
2022-07-08 23:28:18 +0700

    fix model image if user has image

--- app/Http/Controllers/ModelController.php
+++ app/Http/Controllers/ModelController.php
@@ -51,7 +51,7 @@ class ModelController extends Controller
         ]);
 
         $new_role_id = 2;
-        $validatedData['image'] = auth()->user()->model->id;
+        $validatedData['image'] = auth()->user()->image;
         User::where('id',$request->user_id)->update(array('role_id'=> $new_role_id));
         ModelUser::create($validatedData);
         return redirect('dashboard')->with('message', 'Model Registration Success!');
